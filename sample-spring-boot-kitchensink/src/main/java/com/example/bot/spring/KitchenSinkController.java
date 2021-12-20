8730e56b067712bb8205a428ec8b3ce9890c2b13
poy101 <chonlathan.poy@gmail.com>
2021-12-20 14:45:16 +0700

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -346,7 +346,7 @@ public class KitchenSinkController {
         );
     }
 
-	 private static String readAll(Reader rd) throws IOException {
+	 private  String readAll(Reader rd) throws IOException {
     StringBuilder sb = new StringBuilder();
     int cp;
     while ((cp = rd.read()) != -1) {
@@ -355,13 +355,14 @@ public class KitchenSinkController {
     return sb.toString();
   }
 
-  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+  public  JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
     InputStream is = new URL(url).openStream();
     try {
       BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
       String jsonText = readAll(rd);
       JSONObject json = new JSONObject(jsonText);
       return json;
+ } catch(Exception e){
     } finally {
       is.close();
     }
